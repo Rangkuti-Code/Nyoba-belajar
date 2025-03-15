f54cd7391a60d0b53154b1e17c7ea12f6cb4d927
Rangkuti-code <fahlevy289@gmail.com>
2025-03-15 20:40:05 +0800

    funtion modul

--- functionModul.js
+++ functionModul.js
@@ -1,5 +1,34 @@
-function buatJus(apel, gula) {
-    return `Jus ${apel} apel + ${gula} sendok gula !`;
-}
+// function buatJus(apel, gula) {
+//     return `Jus ${apel} apel + ${gula} sendok gula !`;
+// }
 
-console.log(buatJus(5, 2)); // Output: "Jus 5 apel + 2 sendok gula 🍎!"
+// console.log(buatJus(5, 2)); // Output: "Jus 5 apel + 2 sendok gula 🍎!"
+
+// // Fungsi sebagai variabel
+// const potongApel = function(jumlah) {
+//     return jumlah * 4; // 1 apel dipotong jadi 4 🍴
+// };
+
+// // Fungsi sebagai parameter (higher-order function)
+// function prosesApel(apel, action) {
+//     return action(apel);
+// }
+
+// console.log(prosesApel(3, potongApel)); // 12
+
+// function buatPieApel(apel = 5) {
+//     return `Pie dengan ${apel} apel 🥧`;
+// }
+
+// console.log(buatPieApel()); // "Pie dengan 5 apel 🥧"
+
+
+// let totalApel = 10; // Global scope
+
+// function kurangiApel() {
+//     let apelDikurangi = 2; // Local scope
+//     totalApel -= apelDikurangi;
+//     console.log(totalApel); // 8
+// }
+
+// console.log(apelDikurangi); // Error! Variabelnya cuma ada di dalam fungsi
